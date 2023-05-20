17b1a66f1551730de375907f225b542ffd37a3c4
AlfieNavigator <118112707+AlfieNavigator@users.noreply.github.com>
2023-05-20 12:49:23 -0400

    Update penguinfetch.sh

--- penguinfetch.sh
+++ penguinfetch.sh
@@ -2,6 +2,9 @@ chafa
 echo 'Distribucion Linux: Penguin-OS'
 echo 'Kernel:'
 uname -r
-echo 'Arquitectura X86_64'
-echo 'Gestor de paquetes: APT'
+echo 'Arquitectura:'
+getconf LONG_BIT
+echo 'Gestor de paquetes:'
+apt
+pacman
 
